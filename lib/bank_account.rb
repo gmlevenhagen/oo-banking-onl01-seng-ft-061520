0d9973b20e5ffed1795ee70655122521c6139cb5
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-07-01 16:13:00 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,12 @@
 class BankAccount
 
+  attr_accessor :balance, :status
+  attr_reader :name
+
+   def initialize(name, balance=1000, status="open")
+    @name = name
+    @balance = balance
+    @status = status
+  end
+
 end
