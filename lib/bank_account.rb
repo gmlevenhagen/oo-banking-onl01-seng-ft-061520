9723bab6a9901205bb6ed289aa562affe4a21154
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-07-01 16:13:24 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -9,4 +9,20 @@ class BankAccount
     @status = status
   end
 
+  def deposit(amount)
+     self.balance += amount
+   end
+
+    def withdrawal(amount)
+     self.balance -= amount
+   end
+
+    def display_balance
+     "Your balance is $#{self.balance}."
+   end
+
+    def valid?
+     self.status == "open" && self.balance > 0 ? true : false
+   end
+
 end
